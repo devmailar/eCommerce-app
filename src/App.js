a5b7e454c86e8aed92baf87f9092d7dfa9c83fac
devmailar <99177879+leokoivunen@users.noreply.github.com>
2022-03-25 13:56:12 +0200

    Setup e-commerce project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./assets/css/App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>E-Commerce Software</h1>
+      </header>
+
+      <nav className="App-nav">
+        <h1>Navbar</h1>
+      </nav>
+    </div>
+  );
+}
